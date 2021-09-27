7e11006e55e36cc61afcca5a6acdb0c6ca86a2d8
pgeorge1324 <985471009@qq.com>
2021-09-27 15:33:46 +0800

    not inorder;

--- src/components/WaterFall.vue
+++ src/components/WaterFall.vue
@@ -1,5 +1,5 @@
 <template>
-    <div ref="container" style="width: 100%;">
+    <div ref="container" style="width: 100%;display: flex;flex-wrap: wrap">
         <waterfall-item v-for="(item,idx) in items" :key="idx" :width="item.width" :height="item.height"
                         :ratio="item.ratio"
                         :isLast="item.isLast?item.isLast:false"></waterfall-item>
@@ -30,36 +30,54 @@
         },
         methods: {
             renderItems(data, width, gap) {
-                const Data = JSON.parse(JSON.stringify(data));
-                const LeastRow = Math.floor(width / WaterfallWidth);
+                const Data = JSON.parse(JSON.stringify(data)).map((item, idx) => {
+                    return Object.assign(item, {
+                        index: idx,
+                        vWidth: WaterfallHeight * item.width / item.height
+                    })
+                });
+                Data.sort((a, b) => {
+                    return b.vWidth - a.vWidth;
+                });
 
-                let i = 0, widthSum = 0, cnt = 0;
-                while (i < Data.length) {
-                    let prev = widthSum;
-                    widthSum += WaterfallHeight / Data[i].height * Data[i].width;
-                    if ((cnt >= LeastRow && widthSum > width - cnt * gap) || i === Data.length - 1 || i === Data.length - LeastRow) {
-                        let oldRatio = prev / (width - gap * (cnt - 1));
-                        let ratio = widthSum / (width - gap * cnt);
-                        if ((1 - oldRatio) < (ratio - 1)) {
-                            for (; cnt > 0; --cnt) {
-                                Data[i - cnt].ratio = oldRatio;
-                            }
-                            Data[i - 1].isLast = true;
-                            widthSum -= prev;
-                            cnt = 0;
-                        } else {
-                            for (; cnt >= 0; --cnt) {
-                                Data[i - cnt].ratio = ratio;
-                            }
-                            Data[i].isLast = true;
-                            widthSum = 0;
-                            cnt = -1;
-                        }
-                    }
-                    ++cnt;
-                    ++i;
+                const LeastRow = Math.floor(width / WaterfallWidth);
+                let sum = Data.reduce((prev, cur) => prev + cur.vWidth, 0);
+                let group = Math.round(sum / (width - LeastRow * gap));
+
+                let temp = new Array(group);
+                let cntTemp = new Array(group);
+                for (let i = 0; i < group; ++i) {
+                    temp[i] = [];
+                    cntTemp[i] = 0;
                 }
-                return Data;
+
+                function getMinGroupIdx(arr) {
+                    let res = arr.reduce((a, b, idx) => {
+                        if (a.v > b) {
+                            a.v = b;
+                            a.idx = idx;
+                        }
+                        return a;
+                    }, {idx: 0, v: Number.MAX_SAFE_INTEGER});
+                    return res.idx;
+                }
+
+                Data.forEach((item) => {
+                    let groupIndex = getMinGroupIdx(cntTemp);
+                    temp[groupIndex].push(item);
+                    cntTemp[groupIndex] += item.vWidth;
+                });
+
+                return temp.reduce((res, arr, idx) => {
+                    let ratio = cntTemp[idx] / (width - gap * (arr.length - 1));
+                    arr.forEach(it => {
+                        it.ratio = ratio
+                    });
+                    arr.sort((a,b)=>a.index - b.index);
+                    arr[arr.length - 1].isLast = true;
+                    res.push(...arr);
+                    return res;
+                }, []);
             }
         },
     }
--- src/settings/Waterfall.js
+++ src/settings/Waterfall.js
@@ -1,2 +1,2 @@
-export const WaterfallHeight = 200;
+export const WaterfallHeight = 300;
 export const WaterfallWidth = 400;
